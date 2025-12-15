a08aefee38f7e09f130255fc9ac44db96414e9a4
Rishi2013H <pandeyrajiv022@gmail.com>
2025-12-15 21:38:22 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,6 @@
+import datetime
+current_time=datetime.datetime.now()
+print("time now at which green meridian is ",end="")
+print(current_time)
+import calendar
+print("\n",calendar.calendar(2021))
